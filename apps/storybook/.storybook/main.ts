b9e8e73c144e5a88f9bfae59a84bab4862ba152b
Wojciech Malarski <w.malarski@gmail.com>
2024-10-29 22:30:32 +0100

    cleans up new storybook

---
+++ apps/storybook/.storybook/main.ts
@@ -0,0 +1,24 @@
+import { dirname, join } from "node:path";
+import type { StorybookConfig } from "storybook-solidjs-vite";
+/**
+ * This function is used to resolve the absolute path of a package.
+ * It is needed in projects that use Yarn PnP or are set up within a monorepo.
+ */
+function getAbsolutePath(value) {
+	return dirname(require.resolve(join(value, "package.json")));
+}
+
+const config: StorybookConfig = {
+	stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
+	addons: [
+		getAbsolutePath("@storybook/addon-links"),
+		getAbsolutePath("@storybook/addon-essentials"),
+		getAbsolutePath("@chromatic-com/storybook"),
+		getAbsolutePath("@storybook/addon-interactions"),
+	],
+	framework: {
+		name: "storybook-solidjs-vite",
+		options: {},
+	},
+};
+export default config;
