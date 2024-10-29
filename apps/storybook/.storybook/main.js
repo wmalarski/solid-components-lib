d5b192a5c8a46c995ddd8381a5cb8ed48336f6c0
Wojciech Malarski <w.malarski@gmail.com>
2024-10-29 22:11:07 +0100

    adds second storybook project

--- apps/storybook/.storybook/main.js
+++ apps/storybook/.storybook/main.js
@@ -1,19 +1,16 @@
-import type { StorybookConfig } from "storybook-solidjs-vite";
-
 import { join, dirname } from "path";
 
 /**
  * This function is used to resolve the absolute path of a package.
  * It is needed in projects that use Yarn PnP or are set up within a monorepo.
  */
-function getAbsolutePath(value: string): any {
+function getAbsolutePath(value) {
   return dirname(require.resolve(join(value, "package.json")));
 }
-const config: StorybookConfig = {
-  stories: [
-    "../stories/**/*.mdx",
-    "../stories/**/*.stories.@(js|jsx|mjs|ts|tsx)",
-  ],
+
+/** @type { import('storybook-solidjs-vite').StorybookConfig } */
+const config = {
+  stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
   addons: [
     getAbsolutePath("@storybook/addon-links"),
     getAbsolutePath("@storybook/addon-essentials"),
--- packages/ui/tsup.config.ts
+++ packages/ui/tsup.config.ts
@@ -2,15 +2,15 @@ import { defineConfig } from "tsup";
 
 export default defineConfig((options) => ({
 	entryPoints: [
-		"src/index/button.tsx",
-		"src/dialog/dialog.tsx",
+		"src/components/button.tsx",
+		"src/components/dialog.tsx",
 		"src/styles.css",
-		"src/utils/cn.ts",
+		"src/utils.ts",
 	],
 	format: ["cjs", "esm"],
 	dts: true,
 	sourcemap: true,
-	external: ["react"],
+	external: ["solid-js"],
 	treeshake: true,
 	splitting: true,
 	minify: true,
