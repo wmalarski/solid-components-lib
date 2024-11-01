9f68866486787472c32b4f452fedb94f66580bb8
Wojciech Malarski <w.malarski@gmail.com>
2024-11-01 11:37:30 +0100

    adds bundle analyzers

--- apps/web/app.config.ts
+++ apps/web/app.config.ts
@@ -1,3 +1,8 @@
 import { defineConfig } from "@solidjs/start/config";
+import { visualizer } from "rollup-plugin-visualizer";
 
-export default defineConfig({});
+export default defineConfig({
+	vite: {
+		plugins: [visualizer()],
+	},
+});
--- packages/ui/rollup.config.mjs
+++ packages/ui/rollup.config.mjs
@@ -1,10 +1,11 @@
 import autoprefixer from "autoprefixer";
 import postcss from "rollup-plugin-postcss";
+import { visualizer } from "rollup-plugin-visualizer";
 import withSolid from "rollup-preset-solid";
 import tailwindcss from "tailwindcss";
 
 export default withSolid([
-	{ input: "src/index.tsx", targets: ["esm", "cjs"] },
+	{ input: "src/index.tsx", targets: ["esm", "cjs"], plugins: [visualizer()] },
 	{
 		input: "src/styles.css",
 		output: { file: "dist/styles.css" },
